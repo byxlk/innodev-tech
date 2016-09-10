264c28137b29c9a83e7dbb334ace60bc240d34a2
byxlk <313424410@qq.com>
2016-09-10 11:35:33 +0800

    fix starphone-test director name as starphone-demo
    Signed-off-by: byxlk <313424410@qq.com>

---
+++ package/wpcore-starphone/starphone-demo/src/si3050_api.h
@@ -0,0 +1,13 @@
+#ifndef SI3050_API_H
+#define SI3050_API_H
+
+void si3050_get_ver_info(void);
+
+void si3050_hw_reset(void);
+
+void si3050_sys_init(void);
+
+void si3050_power_up_si3019(void);
+
+#endif
+
